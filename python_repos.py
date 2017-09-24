2f0c48bbb5a7a6843c97fd8c613df2200dbe721d
Saisai Yang <1178850242@qq.com>
2017-09-24 12:40:30 +0800

    使用Web API,使用pygal可视化github仓库,另外修改python_repos.py，使description显示（str()强制转换）

--- python_repos.py
+++ python_repos.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import requests
 import pygal
 
@@ -8,21 +9,22 @@ from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 url = 'https://api.github.com/search/repositories'
 url += '?q=language:python&sort=stars'
 r = requests.get(url)
-print('Status code: ' + str(r.status_code))
+print('Status code: ', str(r.status_code))
 response_dict = r.json()
-print('Total repositories: ' + str(response_dict['total_count']))
+print('Total repositories: ', str(response_dict['total_count']))
 
 # 研究有关仓库的信息
 repo_dicts = response_dict['items']
 names, plot_dicts = [], []  # 创建空列表
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
+
     plot_dict = {
-            'value':  repo_dict['stargazers_count'],
-            'label': repo_dict['description'],
-            'http_address': repo_dict['html_url'],
+            'value': repo_dict['stargazers_count'],
+            'xlink': repo_dict['html_url'],
+            'label': str(repo_dict['description']),
             }
-    plot_dicts.append(plot_dict['value'])
+    plot_dicts.append(plot_dict)
 
 # 可视化
 my_style = LS('#333366', base_style=LCS)
@@ -31,8 +33,9 @@ my_config.x_label_rotation = 45
 my_config.show_legend = False
 my_config.title_font_size = 24
 my_config.label_font_size = 14
+my_config.major_label_font_size = 18
 my_config.truncate_lablel = 15
-my_config.show_y_guides = True
+my_config.show_y_guides = False
 my_config.width = 1000
 
 # 创建图表
